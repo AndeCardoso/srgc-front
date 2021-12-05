c02bdca9d01f19be34999f3c5bc9022e61992c30
AndeCardoso <andersoncardoso.dev@gmail.com>
2021-12-05 19:54:51 -0300

    change

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const BASE_API = "http://18.224.213.60:5000";
 
 const api = axios.create ({
-    baseURL: BASE_API,
+    baseURL: "http://18.224.213.60:5000",
 });
 
 export default api;
